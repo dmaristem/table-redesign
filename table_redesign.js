2d4eed6ff5a84ee34d980de7526211cadb8b04e5
Maria Duong <maria.duong.007@gmail.com>
2018-06-04 10:57:56 -0400

    New filtering mechanism, under filterSearch()
    looping through rows instead of through columns to enable multi-search

--- table_redesign.js
+++ table_redesign.js
@@ -56,7 +56,7 @@ function getIdArr(){
 
 //get selected comparison values array of second select forms
 function getCompArr(){
-  alert('getCompArr is firing');
+  // alert('getCompArr is firing');
   var cId, c, cVal;
   var cArray = [];
   $(".comparison-class").each(function(m){
@@ -73,7 +73,7 @@ function getCompArr(){
 
 //get input values in the form of an array (3rd column in filter modal content)
 function getInputArr(){
-  alert("getInputId is firing");
+  // alert("getInputId is firing");
   var inpId, iVal;
   var iArray = [];
   $(".input-class").each(function(p){
@@ -126,140 +126,227 @@ function getInputArr(){
    }
  }
 
- function findCopies(){
+ // function findCopies(){
+ //
+ // }
+function filterSearch(){
+  var compVal = getCompArr();
+  var selVal = getSelArr();
+  var inputArr = getInputArr();
+  
+  //Select the table
+  var table = document.getElementById("team-apps");
 
- }
+  //Select the first row of the table
+  // firstRow = table.getElementsByTagName("tr")[0]; //use next line instead
+  var firstRow = table.rows[0];
 
-  function filter(){
-    alert('filter is firing');
-    var table, input, firstRow, header, i, j, k, n, p,f,g, numCells, numRows, tr, td;
-    var saveK=[];
-    // var kVal = [];
+  //Count the number of cells in the first row (length of header row)
+  var numCells = firstRow.cells.length;
 
-    // var noRepl = [];
-    // var compArr = ["On", "After", "Before", "Between", "Matches", "Contains", "Starts with",
-    // "Pending", "Accepted", "Rejected", "Cohort 2017", "Cohort 2018", "Cohort 2019",
-    // "Equals", "Greater than", "Less than"];
-    var compVal = getCompArr();
-    console.log("compVal is: " + compVal);
-    var selVal = getSelArr();
-    console.log("selVal is: " + selVal);
-    var inputArr = getInputArr();
-   //Select the table
-   table = document.getElementById("team-apps");
+  //Select the table header cells
+  var header = firstRow.getElementsByTagName("th");
+  // header = firstRow;
 
-   //Select the first row of the table
-   // firstRow = table.getElementsByTagName("tr")[0]; //use next line instead
-   firstRow = table.rows[0];
+  //Count the number of rows in the table
+  var numRows = table.rows.length - 1;
 
-   //Count the number of cells in the first row (length of header row)
-   numCells = firstRow.cells.length;
+  var i, j, k, td, tr, saveRow;
 
-   //Select the table header cells
-   header = firstRow.getElementsByTagName("th");
-   // header = firstRow;
-
-   //Get the value of the user input
-   input = document.getElementById("user-input").value;
-   // console.log(input);
-
-   //Count the number of rows in the table
-   numRows = table.rows.length - 1;
-   // rows = table.getElementsByTagName('tbody')[0].rows.length;    also works in lieu of previous line
-
-   //loop through the header row
-    for(i=0;i<numCells;i++){
-      saveK.push(saveK);
-      //Loop through the selected values of the first selected forms
-       for(j=0; j<selVal.length; j++){ //selArr not defined
-       //If the selected value matches the header name
-       if(header[i].textContent == selVal[j]){
-       alert('getSel matches a header data');
-        //numRows - 1 because we're not counting the footer row
-        //go into the specific data cell
-
-
-         for(k=1; k<=numRows-1; k++){
-             tr = table.rows[k];
-             td = tr.cells[i]; //rows of that column
-
-             //loop through the length of the array with selected comparison values
-             // for(n=0;n<compVal.length;n++){
-
-               //Comparison options
-               if((compVal[j] == "Equals" || compVal[j] == "Matches" || compVal[j] == "On") && (td.textContent == inputArr[j])){
-                saveK.push(k);
-              }else if((compVal[j] == "Greater than") && (td.textContent > inputArr[j])){
-                saveK.push(k);
-             }else if((compVal[j] == "Less than") &&(td.textContent < inputArr[j])) {
-                saveK.push(k);
-             }else if((compVal[j] == "Not equal to") && (td.textContent !== inputArr[j])){
-                saveK.push(k);
-             }else if((compVal[j] == "Pending") && (td.textContent == "Pending")){
-                 saveK.push(k);
-             }else if((compVal[j] == "Accepted") && (td.textContent == "Accepted")){
-                 saveK.push(k);
-             }else if((compVal[j] == "Rejected") && (td.textContent == "Rejected")){
-                 saveK.push(k);
-             }else if((compVal[j] == "Contains") && (td.textContent.includes(inputArr[j]))){
-                 saveK.push(k);
-             }else if((compVal[j] == "Starts with") && (td.textContent.startsWith(inputArr[j][0]))){
-                 saveK.push(k);
-             }else{
-                 tr.style.display = "none";
-             }
+  // loop through each row in the table
+  //start i at 1 bc we're skipping the header row
+  // numRows - 1 bc we're skipping the footer row - NVM, remove the -1
+  for(i=1; i<numRows; i++){
+    console.log("i for loop running " + i + " times");
+    tr = table.rows[i];
+    //for each row, loop through each data cell (loop through column)
+    //start j at 2 because we're skipping the column with checkbox and ID
+    saveRow = [];
+    for(j=2;j<numCells;j++){
+      console.log("j for loop running " + j + " times");
+      td = tr.cells[j];
+      for(k=0;k<selVal.length;k++){
+        console.log("k for loop running " + k + " times");
+        if(header[j].textContent == selVal[k]){
+        console.log("textContent == selVal[k]");
+          //Comparison options
+          if((compVal[k] == "Matches" || compVal[k] == "On") && (td.textContent == inputArr[k])){
+           saveRow.push(i);
+         }else if((compVal[k] == "Equals") && (Number(td.textContent) == Number(inputArr[k]))){
+            saveRow.push(i);
+          }else if((compVal[k] == "Greater than") && (Number(td.textContent) > Number(inputArr[k]))){
+            saveRow.push(i);
+        }else if((compVal[k] == "Less than") &&(Number(td.textContent) < Number(inputArr[k]))) {
+           saveRow.push(i);
+        }else if((compVal[k] == "Not equal to") && (Number(td.textContent) !== Number(inputArr[k]))){
+           saveRow.push(i);
+        }else if((compVal[k] == "Pending") && (td.textContent == "Pending")){
+            saveRow.push(i);
+        }else if((compVal[k] == "Accepted") && (td.textContent == "Accepted")){
+            saveRow.push(i);
+        }else if((compVal[k] == "Rejected") && (td.textContent == "Rejected")){
+            saveRow.push(i);
+        }else if((compVal[k] == "Contains") && (td.textContent.includes(inputArr[k]))){
+            saveRow.push(i);
+        }else if((compVal[k] == "Starts with") && (td.textContent.startsWith(inputArr[k][0]))){
+            saveRow.push(i);
+         }
+        //else{
+        //     tr.style.display = "none";
+        // }
+        console.log(saveRow);
 
 
-           // }
-           console.log(saveK);
+        }//end of if statement inside k for loop
+      }//end of k for loop
+     if(saveRow.length == selVal.length){
+        tr.style.display = "";
+     }else{
+       tr.style.display = "none";
+     }
+    }//end of j for loop
+  }// end of i for loop
 
-         } //end of k for loop
+}//end of function filterSearch()
 
-          var kVal = [];
-         //loop through the array that holds the row numbers (k) whose values matched input values
-         for(f=0;f<saveK.length;f++){
-           var replVal = [];
-           var gVal = [];
-           // var kVal = [];
-
-           for(g=f+1;g<saveK.length-1;g++){
-             if(saveK[f] == saveK[g]){
-               replVal.push(saveK[g]); //array with all replicated values except the original
-               //get the indice of all replicates for one f loop, except the original
-               gVal.push(g);
-             }
-
-           }//end of g for loop
-
-            if(saveK[f] == saveK[g]){
-            // gVal.push(f); //don't include this; gVal shouldn't have the index of the original value (that has replicates)
-            replVal.push(saveK[f]);
-            kVal.push(saveK[f]); //k values for hiding/showing table rows
-          }
-
-           //hide or display rows
-           if(replVal.length == getSelArr().length){  // the +1 is an issue; if saveK[f] !== saveK[g] and getSelArr().length = 1,
-             //then this if statement would run despite not actually fulfilling the condition; WAIT NO
-             //if savek[f] !== saveK[g], then the g for loop would run again, and the if statement would run again
-             for(m=0;m<kVal.length;m++){
-               tr = table.rows[kVal[m]];
-               tr.style.display = "";
-               }
-             }
-
-           //gets rid of all the replicates (after the original) in saveK ***********************
-           for(h=0;h<gVal.length;h++){
-             saveK.splice(gVal[h], 1);
-           }
-
-
-         } //end of f for loop
-
-     } //end of first if statement
-
-  }//end of j for loop
-}//end of i for loop
- } //end of function filter()
+//   function filter(){
+//     alert('filter is firing');
+//     var table, input, firstRow, header, i, j, k, n, p,f,g, numCells, numRows, tr, td;
+//     // var saveK=[];
+//     var saveK;
+//     // var kVal = [];
+//
+//     // var noRepl = [];
+//     // var compArr = ["On", "After", "Before", "Between", "Matches", "Contains", "Starts with",
+//     // "Pending", "Accepted", "Rejected", "Cohort 2017", "Cohort 2018", "Cohort 2019",
+//     // "Equals", "Greater than", "Less than"];
+//     var compVal = getCompArr();
+//     console.log("compVal is: " + compVal);
+//     var selVal = getSelArr();
+//     console.log("selVal is: " + selVal);
+//     var inputArr = getInputArr();
+//    //Select the table
+//    table = document.getElementById("team-apps");
+//
+//    //Select the first row of the table
+//    // firstRow = table.getElementsByTagName("tr")[0]; //use next line instead
+//    firstRow = table.rows[0];
+//
+//    //Count the number of cells in the first row (length of header row)
+//    numCells = firstRow.cells.length;
+//
+//    //Select the table header cells
+//    header = firstRow.getElementsByTagName("th");
+//    // header = firstRow;
+//
+//    //Get the value of the user input
+//    input = document.getElementById("user-input").value;
+//    // console.log(input);
+//
+//    //Count the number of rows in the table
+//    numRows = table.rows.length - 1;
+//    // rows = table.getElementsByTagName('tbody')[0].rows.length;    also works in lieu of previous line
+//
+//
+//    //loop through the header row
+//     for(i=0;i<numCells;i++){
+//       // saveK.push(saveK);
+//       //Loop through the selected values of the first selected forms
+//        for(j=0; j<selVal.length; j++){ //selArr not defined
+//        //If the selected value matches the header name
+//        if(header[i].textContent == selVal[j]){
+//        alert('getSel matches a header data');
+//         //numRows - 1 because we're not counting the footer row
+//         //go into the specific data cell
+//
+//          saveK = [];
+//          for(k=1; k<=numRows-1; k++){
+//              tr = table.rows[k];
+//              td = tr.cells[i]; //rows of that column
+//
+//              //loop through the length of the array with selected comparison values
+//              // for(n=0;n<compVal.length;n++){
+//
+//                //Comparison options
+//                if((compVal[j] == "Equals" || compVal[j] == "Matches" || compVal[j] == "On") && (td.textContent == inputArr[j])){
+//                 saveK.push(k);
+//               }else if((compVal[j] == "Greater than") && (td.textContent > inputArr[j])){
+//                 saveK.push(k);
+//              }else if((compVal[j] == "Less than") &&(td.textContent < inputArr[j])) {
+//                 saveK.push(k);
+//              }else if((compVal[j] == "Not equal to") && (td.textContent !== inputArr[j])){
+//                 saveK.push(k);
+//              }else if((compVal[j] == "Pending") && (td.textContent == "Pending")){
+//                  saveK.push(k);
+//              }else if((compVal[j] == "Accepted") && (td.textContent == "Accepted")){
+//                  saveK.push(k);
+//              }else if((compVal[j] == "Rejected") && (td.textContent == "Rejected")){
+//                  saveK.push(k);
+//              }else if((compVal[j] == "Contains") && (td.textContent.includes(inputArr[j]))){
+//                  saveK.push(k);
+//              }else if((compVal[j] == "Starts with") && (td.textContent.startsWith(inputArr[j][0]))){
+//                  saveK.push(k);
+//              }else{
+//                  tr.style.display = "none";
+//              }
+//
+//
+//            // }
+//            console.log(saveK);
+//
+//          } //end of k for loop
+//
+//           var kVal = [];
+//          //loop through the array that holds the row numbers (k) whose values matched input values
+//          for(f=0;f<saveK.length;f++){
+//            var replVal = [];
+//            var gVal = [];
+//            // var kVal = [];
+//
+//            for(g=f+1;g<saveK.length-1;g++){
+//              if(saveK[f] == saveK[g]){
+//                replVal.push(saveK[g]); //array with all replicated values except the original
+//                //get the indice of all replicates for one f loop, except the original
+//                gVal.push(g);
+//              }
+//              if(saveK[f] == saveK[g] && g == saveK.length - 2){
+//                replVal.push(saveK[f]);
+//                kVal.push(saveK[f]); //k values for hiding/showing table rows
+//              }
+//
+//            }//end of g for loop
+//
+//           //   if(saveK[f] == saveK[g]){ //PROBLEM - out of the g for loop
+//           //   // gVal.push(f); //don't include this; gVal shouldn't have the index of the original value (that has replicates)
+//           //   replVal.push(saveK[f]);
+//           //   kVal.push(saveK[f]); //k values for hiding/showing table rows
+//           // }
+//
+//            //hide or display rows
+//            if(replVal.length == selVal().length){  // the +1 is an issue; if saveK[f] !== saveK[g] and getSelArr().length = 1,
+//              //then this if statement would run despite not actually fulfilling the condition; WAIT NO
+//              //if savek[f] !== saveK[g], then the g for loop would run again, and the if statement would run again
+//              for(m=0;m<kVal.length;m++){
+//                tr = table.rows[kVal[m]];
+//                tr.style.display = "";
+//                }
+//              }
+//
+//            //gets rid of all the replicates (after the original) in saveK ***********************
+//            for(h=0;h<gVal.length;h++){
+//              saveK.splice(gVal[h], 1);
+//            }
+//
+//            console.log("saveK spliced is: " + saveK);
+//
+//
+//          } //end of f for loop
+//
+//      } //end of first if statement
+//
+//   }//end of j for loop
+// }//end of i for loop
+//  } //end of function filter()
 
  //Not working
  // function hideInputTextbox(){
@@ -275,7 +362,7 @@ function getInputArr(){
  $("#filter").on("load", getModal);
  getModal();
   $(".select-class").change(changeCompFilter);
-  $('#submit-filter').click(filter);
+  $('#submit-filter').click(filterSearch);
   // $('#submit-filter').click(getCompArr);
   // $('#submit-filter').click(getInputArr);
 
